cc3f1f8726c3ffab5547d0a1b90181198e703f52
Owen Badzinski <badzinso@oregonstate.edu>
2026-05-14 21:17:40 -0700

    inital version of service.py

---
+++ service.py
@@ -0,0 +1,45 @@
+import zmq
+import json
+
+database = {}
+
+# set up ZeroMQ
+context = zmq.Context()
+socket = context.socket(zmq.REP) # rep = reply, the listener
+socket.bind("tcp://*:5555")
+
+while True:
+    message = socket.recv_json()
+    if message['context'] == "login":
+        if message['username'] in database:
+            if message['password'] == database[message['username']]['password']:
+                socket.send_json({"status": True, "message": "Login successful" })
+            else:
+                socket.send_json({"status": False, "message": "Wrong password"})
+        else:
+            socket.send_json({"status": False, "message": "wrong username"})
+    elif message['context'] == "register":
+        username = message['username']
+        password = message['password']
+        birth_date = message['birth_date']
+        database[username] = {"password": password, "birth_date": birth_date}
+        socket.send_json({"status": True, "message": "successfully registered with username and password"})
+    elif message['context'] == "forgot_password":
+
+        username = message['username']
+        answer = message['birth_date'] # using birthdate to verify the user
+        new_password = message['new_password']
+        if username in database:
+            if database[username]['birth_date'] == answer:
+                database[username]['password'] = new_password
+
+                socket.send_json({"status": True, "message": "Password reset successful"})
+
+            else:
+                socket.send_json({"status": False, "message": "Security answer incorrect"})
+
+        else:
+            socket.send_json({"status": False, "message": "Username not found"})
+
+
+
